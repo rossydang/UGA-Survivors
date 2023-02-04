18c34f93c445b7f0f7f17f810a90324804716ab1
Rossy Dang <rdang31@gatech.edu>
2023-02-04 15:35:29 -0500

    Change player to rigid body

--- Assets/Code Scripts/PlayerMovement.cs
+++ Assets/Code Scripts/PlayerMovement.cs
@@ -4,23 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float runSpeed = 160f;
+    public float runSpeed = 5f;
     // Start is called before the first frame update
+    public Rigidbody2D rb;
+    Vector2 movement;
     void Start()
     {
     }
 
     // Update is called once per frame
     void Update(){
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
 
     }
 
     void FixedUpdate() {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector2.right * horizontalInput *  runSpeed* Time.fixedDeltaTime);
-        Debug.Log(transform.position);
-
-        float vertical = Input.GetAxis("Vertical");
-        transform.Translate(Vector2.up * vertical *  runSpeed* Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * runSpeed * Time.fixedDeltaTime);
     }
 }
