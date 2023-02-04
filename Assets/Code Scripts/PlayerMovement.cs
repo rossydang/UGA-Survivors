afabc42f27224dac11f65ba48b9dd1de751e1d67
Rossy Dang <rdang31@gatech.edu>
2023-02-04 16:01:01 -0500

    1st attempt at pointing towards camera

--- Assets/Code Scripts/PlayerMovement.cs
+++ Assets/Code Scripts/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     public Rigidbody2D rb;
     Vector2 movement;
+    Vector2 mousePos;
+    public Camera cam;
     void Start()
     {
     }
@@ -16,10 +18,14 @@ public class PlayerMovement : MonoBehaviour
     void Update(){
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
     }
 
     void FixedUpdate() {
         rb.MovePosition(rb.position + movement * runSpeed * Time.fixedDeltaTime);
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
     }
 }
