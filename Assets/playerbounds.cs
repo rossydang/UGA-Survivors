9fc5008a0a3590f05c72d680f5f07af798e0ac89
jorge patino <jmp17450@uga.edu>
2023-02-05 01:42:59 -0500

    rossy be sure to pull i made walls for the map

---
+++ Assets/playerbounds.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerbounds : MonoBehaviour
+{
+    public float rPlayerBound;
+    public float lPlayerBound; 
+    public float upPlayerBound;
+    public float lowPlayerBound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
